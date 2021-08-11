f348578a9be444995c19f3465fef60fde178a461
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-08-11 12:15:57 +0530

    Fusion configuration changes

--- src/main/java/com/scoperetail/fusion/messaging/kafka/KafkaConfig.java
+++ src/main/java/com/scoperetail/fusion/messaging/kafka/KafkaConfig.java
@@ -1,12 +1,31 @@
 package com.scoperetail.fusion.messaging.kafka;
 
+/*-
+ * *****
+ * fusion-messaging-kafka
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.springframework.beans.factory.InitializingBean;
@@ -17,15 +36,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.util.CollectionUtils;
-
-import com.scoperetail.fusion.messaging.adapter.JmsProvider;
-import com.scoperetail.fusion.messaging.config.Adapter;
-import com.scoperetail.fusion.messaging.config.Broker;
-import com.scoperetail.fusion.messaging.config.FusionConfig;
-import com.scoperetail.fusion.messaging.config.KafkaProducer;
-import com.scoperetail.fusion.messaging.config.KafkaSecurityConfig;
+import com.scoperetail.fusion.config.Adapter;
+import com.scoperetail.fusion.config.Broker;
+import com.scoperetail.fusion.config.Broker.JmsProvider;
+import com.scoperetail.fusion.config.FusionConfig;
+import com.scoperetail.fusion.config.KafkaProducer;
+import com.scoperetail.fusion.config.KafkaSecurityConfig;
 import com.scoperetail.fusion.messaging.kafka.adapter.out.KafkaMessageSender;
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
